bf3ce82522ef286bd8c9400377ae5e2eb1b06e82
Alina Custură <68774898+alinaai8@users.noreply.github.com>
2023-07-09 13:15:51 +0300

    UI and CleanUp

--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,26 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Ball ball;
     private int _playerScore;
     private int _computerScore;
+    public Text playerScoreText;
+    public Text computerScoreText;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
 
     public void PlayerScores()
     {
         _playerScore++;
-        Debug.Log(_playerScore);
+        this.playerScoreText.text = _playerScore.ToString();
 
-        this.ball.ResetPosition();
+        ResetRound();
     }
 
     public void ComputerScore()
     {
         _computerScore++;
-        Debug.Log(_computerScore);
+        this.computerScoreText.text = _computerScore.ToString();
 
-        this.ball.ResetPosition();
+        ResetRound();
     }
 
+    private void ResetRound()
+    {
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
 
 }
