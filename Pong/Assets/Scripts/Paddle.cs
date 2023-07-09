32a1658f2a39adbe6f0c7835d33d3e9e58d540d1
Alina Custură <68774898+alinaai8@users.noreply.github.com>
2023-07-09 11:21:43 +0300

    Player Paddle

---
+++ Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+    public float speed = 10.0f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
